19bccb8cb3850dfeaab865bf8630e548e916fd9c
Luke <contact@chiarchiaooo.dev>
2022-02-01 20:19:43 +0100

    Blocker Fix update

--- src/main/java/it/chiarchiaooo/commandblocker/Commands/ReloadCommand.java
+++ src/main/java/it/chiarchiaooo/commandblocker/Commands/ReloadCommand.java
@@ -15,20 +15,23 @@ public class ReloadCommand implements CommandExecutor {
     String prefix = Main.getInstance().getConfig().getString("prefix");
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Logger log = getLogger();
-
-        if (sender instanceof Player && (sender.hasPermission("cmdblock.reload") || sender.isOp())) {
-            log.info("Reloading configs...");
-            Main.getInstance().onDisable();
-            Main.getInstance().onEnable();
-            Player p = (Player) sender;
-            log.info(ChatColor.translateAlternateColorCodes('&',"&aConfigs reloaded "));
-            p.sendMessage(ChatColor.translateAlternateColorCodes('&',prefix + " &aPlugin ricaricato con successo"));
+        if (sender instanceof Player) {
+            if (sender.hasPermission("vanillacore.reload") || sender.isOp()) {
+                Main.getInstance().getLogger().info("Reloading configs...");
+                Main.getInstance().reloadConfig();
+                Main.getInstance().saveConfig();
+                Main.getInstance().onDisable();
+                Main.getInstance().onEnable();
+                Main.getInstance().getLogger().info(ChatColor.translateAlternateColorCodes('&', "&aConfig reloaded"));
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aPlugin reloaded Successfully"));
+            }
         } else {
-            log.info("Reloading configs...");
+            Main.getInstance().getLogger().info("Reloading configs...");
+            Main.getInstance().reloadConfig();
+            Main.getInstance().saveConfig();
             Main.getInstance().onDisable();
             Main.getInstance().onEnable();
-            log.info(ChatColor.translateAlternateColorCodes('&',"&aConfigs reloaded"));
+            Main.getInstance().getLogger().info(ChatColor.translateAlternateColorCodes('&', "&aConfig reloaded"));
         }
         return true;
     }
