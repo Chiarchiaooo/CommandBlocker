9720be236a21ac78d856ba6d952d08319bb98274
Luke <contact@chiarchiaooo.dev>
2022-04-29 17:45:00 +0200

    update 5.1

--- src/main/java/it/chiarchiaooo/commandblocker/Listeners/CommandEvent.java
+++ src/main/java/it/chiarchiaooo/commandblocker/Listeners/CommandEvent.java
@@ -14,32 +14,26 @@ import java.util.Map;
 
 public class CommandEvent implements Listener {
 
-    private FileConfiguration config;
+    private final FileConfiguration config;
+    Map<String, String> commandBypasses = new HashMap<>();
+    final String genbypass = "cmdblock.bypass.*";
+    boolean block = true;
 
     public CommandEvent(Main pl) {
         this.config = pl.getConfig();
     }
-
-     List<String> cmds = config.getStringList("allowed-cmds");
-     List<String> staffcmds = config.getStringList("single-allowed-cmds");
-     String blockmsg = config.getString("blocked-command-message");
-     boolean BlockEnable = config.getBoolean("enabled");
-     String prefix = config.getString("prefix");
-     Map<String, String> commandBypasses = new HashMap<>();
-     final String genbypass = "cmdblock.bypass.*";
-     boolean block = true;
+    
 
     @EventHandler
     public void CmdEvent(PlayerCommandPreprocessEvent event){
-        if (BlockEnable) {
+        if (config.getBoolean("enabled")) {
             if (event.getPlayer().hasPermission(genbypass)) {
                 return;
             }
-            String msg = blockmsg.replace("%prefix%",prefix).replace("%player%",event.getPlayer().getDisplayName()).replace("%command%",event.getMessage());
-            for (String staffalcmds : staffcmds) {
+            for (String staffalcmds : config.getStringList("single-allowed-cmds")) {
                 commandBypasses.put(staffalcmds, "cmdblock.bypass." + staffalcmds.substring(1));
             }
-            for (String alcmd : cmds) {
+            for (String alcmd : config.getStringList("allowed-cmds")) {
                 if (StringUtils.startsWithIgnoreCase(event.getMessage(), alcmd)) {
                     block = false;
                     break;
@@ -54,7 +48,7 @@ public class CommandEvent implements Listener {
             }
             if (block) {
                 event.setCancelled(true);
-                event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&',msg));
+                event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&',config.getString("blocked-command-message").replace("%prefix%",config.getString("prefix")).replace("%player%",event.getPlayer().getDisplayName()).replace("%command%",event.getMessage())));
             }
         }
     }
