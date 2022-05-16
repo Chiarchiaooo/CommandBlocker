a36718e6b2a6c379f0d4be65e8b69d269b7e8b03
Luke <contact@chiarchiaooo.dev>
2022-05-16 17:51:57 +0200

    update 1.7 (Added PlaceholderAPI)

--- src/main/java/it/chiarchiaooo/commandblocker/utils/TabCompleteHandler.java
+++ src/main/java/it/chiarchiaooo/commandblocker/utils/TabCompleteHandler.java
@@ -1,25 +1,20 @@
 package it.chiarchiaooo.commandblocker.utils;
 
-import it.chiarchiaooo.commandblocker.Main;
-import it.chiarchiaooo.commandblocker.utils.CommandHandler;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class TabCompleteHandler implements TabCompleter {
 
-    private final HashMap<String, CommandHandler.CommandInterface> commands;
 
-    public TabCompleteHandler(Main pl) {
-        this.commands = pl.commands;
-    }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
-        if (args.length ==1) return new ArrayList<>(commands.keySet());
+    public List<String> onTabComplete(@NotNull CommandSender sender, Command cmd, String alias, String[] args) {
+        if (args.length ==1) return new ArrayList<>(CommandHandler.commands.keySet());
         return new ArrayList<>();
     }
 }
