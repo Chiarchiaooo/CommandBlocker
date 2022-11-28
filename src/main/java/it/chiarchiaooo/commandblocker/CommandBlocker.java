d2c2d696b2d6bd76d74d7b6497c217654f139cbf
Alberto Migliorato <dev@albemiglio.it>
2022-11-29 00:01:54 +0100

    Modifications

--- src/main/java/it/chiarchiaooo/commandblocker/CommandBlocker.java
+++ src/main/java/it/chiarchiaooo/commandblocker/CommandBlocker.java
@@ -1,38 +1,35 @@
 package it.chiarchiaooo.commandblocker;
 
-
-import it.chiarchiaooo.commandblocker.Listeners.Command;
-import it.chiarchiaooo.commandblocker.Listeners.TabComplete;
-import it.chiarchiaooo.commandblocker.Managers.Commands;
-import it.chiarchiaooo.commandblocker.Managers.Config;
-import it.chiarchiaooo.commandblocker.Managers.TabCompleter;
-import it.chiarchiaooo.commandblocker.utils.Messages;
-import it.chiarchiaooo.commandblocker.utils.Vars;
+import it.chiarchiaooo.commandblocker.listeners.CommandListener;
+import it.chiarchiaooo.commandblocker.listeners.TabSuggestListener;
+import it.chiarchiaooo.commandblocker.commands.MainCommand;
+import it.chiarchiaooo.commandblocker.services.ConfigService;
+import it.chiarchiaooo.commandblocker.services.MsgService;
+import it.chiarchiaooo.commandblocker.services.VarService;
 import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter
 public final class CommandBlocker extends JavaPlugin {
 
-    @Getter static CommandBlocker instance;
-
-    @Getter static Vars vars = new Vars();
-
-    @Getter static Messages msgHandler = new Messages();
-
-    @Setter Config config;
+    @Getter
+    private static CommandBlocker instance;
+    private VarService varService;
+    private MsgService msgService;
+    private ConfigService configService;
 
 
     public void onEnable() {
+        instance = this;
         getLogger().info("");
         getLogger().info("§6§lCommandBlocker");
         getLogger().info("");
         getLogger().info("§eMade by §6Chiarchiaooo§7 (§6~Luke#0002§7)");
         getLogger().info("");
 
-        setConfig(new Config());
+        setupServices();
         setupEvents();
         setupCommands();
 
@@ -42,18 +39,24 @@ public final class CommandBlocker extends JavaPlugin {
         getLogger().info("");
     }
 
+    private void setupServices() {
+        varService = new VarService(this);
+        msgService = new MsgService(this);
+        configService = new ConfigService(this);
+    }
+
     @SuppressWarnings("all")
     public void setupCommands() {
-        Commands handler = new Commands(); //register cmds
-        getCommand("cmdblock").setExecutor(handler);
-        getCommand("cmdblock").setTabCompleter(new TabCompleter());
+        getCommand("cmdblock").setExecutor(new MainCommand(this));
+        getCommand("cmdblock").setTabCompleter(new TabSuggestListener(this));
 
         getLogger().info("Comandi registrati");
     }
 
     public void setupEvents() {
-        getServer().getPluginManager().registerEvents(new Command(), this);
-        if (vars.getIsTabBlockingEnabled()) getServer().getPluginManager().registerEvents(new TabComplete(), this);  // Make sure tab blocker is enabled from
+        getServer().getPluginManager().registerEvents(new CommandListener(this), this);
+        if (varService.isTabBlockingEnabled()) getServer().getPluginManager()
+                .registerEvents(new TabSuggestListener(this), this);  // Make sure tab blocker is enabled from
 
         getLogger().info("Comandi registrati");// config
     }
