2d0f1371e478e60dc9b591b7f6dbbc38c73e1eeb
Luke <contact@chiarchiaooo.dev>
2022-11-30 07:15:08 +0100

    update 10, Code Cleanup

--- src/main/java/it/chiarchiaooo/commandblocker/listeners/CommandListener.java
+++ src/main/java/it/chiarchiaooo/commandblocker/listeners/CommandListener.java
@@ -8,8 +8,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CommandListener implements Listener {
@@ -26,7 +28,7 @@ public class CommandListener implements Listener {
     }
 
 
-    @EventHandler
+    @EventHandler (ignoreCancelled = true)
     public void CmdEvent(PlayerCommandPreprocessEvent e) {
         Player p = e.getPlayer();
 
@@ -38,15 +40,13 @@ public class CommandListener implements Listener {
                 || varService.getCmdWhitelist().contains(cmd))
             return;
 
-        else if (hasPermission(p, cmd)) return;
+        else if (hasCmdPermission(p, cmd) || (varService.isCommandGroupsEnabled() && isGroupCmd(p, cmd))) return;
 
-        else if (varService.isCommandGroupsEnabled() && isGroupCmd(p, cmd))
-            return;
-        // first check: are command groups enabled?, second check: is the player in a group?
-        // if yes, is the command in the group cmd list?
 
         e.setCancelled(true);
-        p.sendMessage(msgService.sendMsg(p, varService.getCmdBlockedMsg()).replaceAll("%command%|%cmd%", cmd));
+        p.sendMessage(msgService.setPlaceholders(p, varService.getCmdBlockedMsg())
+                            .replaceAll("%command%|%cmd%", cmd)
+        );
     }
 
 
@@ -58,7 +58,7 @@ public class CommandListener implements Listener {
      * @return Whether the Cmd is blocked or not
      */
 
-    private boolean hasPermission(Player p, String cmd) {
+    private boolean hasCmdPermission(Player p, String cmd) {
         return p.hasPermission("cmdblock.bypass." + cmd);
     }
 
@@ -69,36 +69,38 @@ public class CommandListener implements Listener {
      * @return The command group
      */
 
-    private String getPlayerCmdGroup(Player p) {
-        List<String> keys = varService.getCmdGroups();
-        for (String group : keys) {
+    private List<String> getGroupCmds(Player p) {
+        for (Map.Entry<String,List<String>> m : varService.getCmdGroupCommands().entrySet()) {
 
-            if (p.hasPermission(Objects.requireNonNull(main.getConfig().getString("groups." + group + ".permission"))))
-                return group;
+            if (p.hasPermission(m.getKey())) return m.getValue();
+
         }
-        return "";
+        return new ArrayList<>();
     }
 
     /**
      * Checks to see if a cmd is blocked using permission groups and regex patterns
      *
-     * @param p   The player to check
+     * @param p The player trying to execute the command
      * @param cmd The cmd the player is trying to execute
      * @return Whether the Cmd is blocked or not
      */
 
     private boolean isGroupCmd(Player p, String cmd) {
-        return !getPlayerCmdGroup(p).isEmpty() && Pattern.compile(cmdsToPattern(getPlayerCmdGroup(p))).matcher(cmd).find();
+        if (getGroupCmds(p).isEmpty()) return false;
+
+        Pattern regexPattern = Pattern.compile( listToPattern(getGroupCmds(p)) );
+        Matcher regexMatcher = regexPattern.matcher(cmd);
+
+        return regexMatcher.find();
     }
 
     /**
-     * Converts a list of cmd into a String regex pattern
+     * Converts a list of cmds into a String regex pattern
      *
-     * @param group The group associated with the list of cmd to be converted
+     * @param groupCmds The commands list to convert
      * @return The list converted into a string regex pattern
      */
 
-    private String cmdsToPattern(String group) {
-        return String.join("|", main.getConfig().getStringList("groups." + group + ".commands"));
-    }
+    private String listToPattern(List<String> groupCmds) {return String.join("|", groupCmds);}
 }
